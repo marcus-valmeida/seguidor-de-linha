96246e791775e14ae6b705900346e8af4bf3c430
marcusvinicius <marcusvinicius23415@gmail.com>
2024-03-26 21:43:52 -0300

    criacao dos tipos de erros

--- controle_pid/controle_pid.ino
+++ controle_pid/controle_pid.ino
@@ -1,4 +1,4 @@
-int sensorD= 11;
+int sensorD = 11;
 int sensorM = 12;
 int sensorE = 13;
 
@@ -10,7 +10,7 @@ int IN3 = 6;
 int IN4 = 7;
 int PWM_A = 8;
 
-sensor [2] = {0,0,0};
+int sensor[3] = {0,0,0};
 int velocidade = 255;
 
 int Ki = 0;
@@ -38,9 +38,8 @@ void setup() {
   pinMode(sensorM, INPUT);
   pinMode(sensorE, INPUT);
 
-
 }
-void curva_90_direita(){
+/*void curva_90_direita(){
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, HIGH);
@@ -57,47 +56,51 @@ void curva_90_esquerda(){
   digitalWrite(IN4, LOW);
   analogWrite(PWM_A, 0.1*velocidade);
   analogWrite(PWM_B, 0.8*velocidade);
-}
-
+}*/
+/*
 void parada(){
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, HIGH);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, HIGH);
 }
-
+*/
 void calculo_erro(){
-  if ((sensor[0] == 0) && (sensor[1] == 1) && (sensor[2] == 0)){// linha reta
-    (erro = 0)
+  if ((sensor[0] == 0) && (sensor[1] == 1) && (sensor[2] == 0)){// linha reta , erro = 0
+    (erro = 0);
   }
-  else if ((sensor[0] == 0) && (sensor[1] == 0) && (sensor[2] ==1)){// leve curva para direita
-
+  else if ((sensor[0] == 0) && (sensor[1] == 0) && (sensor[2] ==1)){// leve curva para direita , erro = 2
+    (erro = 2);
   }
-  else if((sensor[0] == 1) && (sensor[1] == 0) && (sensor[2] == 0)){ // leve curva para esquerda
-
+  else if((sensor[0] == 1) && (sensor[1] == 0) && (sensor[2] == 0)){ // leve curva para esquerda , erro = -2
+    (erro = -2);
   }
-
-  else if((sensor[0] == 0) && (sensor[1] == 1) && (sensor[2] ==1)){ // curva 90 direita
-
+  else if((sensor[0] == 0) && (sensor[1] == 1) && (sensor[2] ==1)){ //  erro = 1
+    (erro = 1);
   }
-  else if ((sensor[0] == 1) && (sensor[1] == 1) && (sensor[2] == 0)){ // curva de 90 esquerda
-
+  else if ((sensor[0] == 1) && (sensor[1] == 1) && (sensor[2] == 0)){ // erro = -1
+    (erro = -1);
   }
 }
 
 void calculo_do_PID(){
   if (erro == 0){
+    //caso esteja no caso ideal.
     I = 0;
   }
   P = erro;
   I = I + erro;
+
   if(I > 255){
+    // caso o erro esteja acima da velocidade maxima.
     I = 255;
   }
 
   else if (I < -255){
+    // caso para o erro não ultrapassar o minimo possivel.
     I = -255;
   }
+  // Realizando o calculo.
   D = erro - erro_anterior;
   PID = (Kp*P) + (Ki*I) + (Kd*D);
   erro_anterior = erro;
@@ -105,36 +108,38 @@ void calculo_do_PID(){
 
 void controle_motores(){
   if (PID >= 0){
-    veloeq = velo_B;
-    velodir = velo_A - PID;
+    veloeq = velocidade;
+    velodir = velocidade - PID;
   }
   else{
-    veloeq = velo_B + PID;
-    velodir = velo_A;
+    veloeq = velocidade + PID;
+    velodir = velocidade;
   }
 
   digitalWrite (IN1, HIGH);
   digitalWrite (IN1, HIGH);
   digitalWrite (IN1, HIGH);
   digitalWrite (IN1, HIGH);
-  analogWrite (PWM_A, velo;
+  analogWrite (PWM_A, velocidade);
   analogWrite (PWM_B, HIGH);
 }
 
 void loop() {
 
-  sensor[0] = DigitalRead(sensorE);
-  sensor[1] = DigitalRead(sensorM);
-  sensor[2] = DigitalRead(sensorD);
+  sensor[0] = digitalRead(sensorE);
+  sensor[1] = digitalRead(sensorM);
+  sensor[2] = digitalRead(sensorD);
 
   Serial.print(" e ");
-  Serial.print(sensor[2]);
-  Serial.print(" m ")
+  Serial.print(sensor[0]);
+  Serial.print(" m ");
   Serial.print(sensor[1]);
   Serial.print(" d ");
-  Serial.print(sensor[0]);
+  Serial.println(sensor[2]);
 
- 
-
+  calculo_erro();
+  calculo_do_PID();
+  controle_motores();
+  
 
 }
